b424291df5c07dc02a7723d75a3d9885b16b364d
katthigokul <gokulkatthi@gmail.com>
2019-07-19 12:52:47 +0530

    Created Test cases for ConsecutiveNumbers

---
+++ src/test/java/com/stackroute/Pe3/ConsecutiveNumbersTest.java
@@ -0,0 +1,41 @@
+package com.stackroute.Pe3;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ConsecutiveSeriesTest {
+    private static final List<Long> CONSECUTIVE_SERIES = Arrays.asList(2020L, 2019L, 2021L, 2022L);
+    private static final List<Long> REPEATED_SERIES = Arrays.asList(2020L, 2019L, 2021L, 2021L);
+    private static final List<Long> NON_CONSECUTIVE_SERIES = Arrays.asList(2019L, 2020L, 2021L, 2023L);
+    private ConsecutiveNumbers consecutiveNumbers;
+
+    @Before
+    public void setUp() {
+        consecutiveSeries = new ConsecutiveNumbers();
+    }
+
+    @Test
+    public void testForConsecutiveSeriesSuccess() {
+        assertTrue(consecutiveNumbers.isSeriesConsecutive(CONSECUTIVE_SERIES));
+    }
+
+    @Test
+    public void testForNonConsecutiveSeries() {
+        assertFalse(consecutiveNumbers.isSeriesConsecutive(NON_CONSECUTIVE_SERIES));
+    }
+    @Test
+    public void testForSeriesWithRepeatedNumbers() {
+        assertFalse(consecutiveNumbers.isSeriesConsecutive(REPEATED_SERIES));
+    }
+
+    @Test
+    public void testForConsecutiveSeriesForInvalidInput() {
+        assertFalse(consecutiveNumbers.isSeriesConsecutive(null));
+    }
+
+}
