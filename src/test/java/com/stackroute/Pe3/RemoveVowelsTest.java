a42562b012f227086270acdeaf0abab0ddfe1634
katthigokul <gokulkatthi@gmail.com>
2019-07-19 12:39:12 +0530

    Created Test cases for RemoveVowels

---
+++ src/test/java/com/stackroute/Pe3/RemoveVowelsTest.java
@@ -0,0 +1,13 @@
+package com.stackroute.Pe3;
+
+import static org.junit.Assert.*;
+
+public class RemoveVowelsTest {
+    private static final List<String> VALID = Arrays.asList("Selena", "Katherine", "Utopia");
+
+    @Test
+    public void testForRemovingLowerCaseVowels() {
+        RemoveVowels removeLowerCaseVowels = new RemoveVowels();
+        assertEquals(Arrays.asList("Sln", "Kthrn", "Utp"), removeLowerCaseVowels.removeLowerCaseVowels(VALID));
+    }
+}
