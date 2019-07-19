8d3c83cdd677e5ad7f3688e1b8ac8b0f82e5f8e8
katthigokul <gokulkatthi@gmail.com>
2019-07-19 12:42:24 +0530

    Created Test cases for RemoveVowels

--- src/test/java/com/stackroute/Pe3/RemoveVowelsTest.java
+++ src/test/java/com/stackroute/Pe3/RemoveVowelsTest.java
@@ -1,11 +1,25 @@
 package com.stackroute.Pe3;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class RemoveVowelsTest {
     private static final List<String> VALID = Arrays.asList("Selena", "Katherine", "Utopia");
+    RemoveVowels removeVowels;
+    @Before
+    public void setUp() {
+        removeVowels = new RemoveVowels();
+    }
+
+    @After
+    public void tearDown() {
+        removeVowels = null;
+    }
+
+
 
     @Test
     public void testForRemovingLowerCaseVowels() {
